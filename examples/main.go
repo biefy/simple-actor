2abbecb92ab75545a73aae6337013c57c5c0518b
Fuyuan Bie <fuyuanbie@Fuyuans-iMac.local>
2018-10-02 01:15:57 -0700

    reformat

--- examples/main.go
+++ examples/main.go
@@ -35,12 +35,13 @@ func main() {
 	})
 
 	x := 2
-	a.Cast(EventAdd, &x, 1)  		// x = 3
+	a.Cast(EventAdd, &x, 1)		// x = 3
 	a.Cast(EventMultiply, &x, 10)	// x = 30
-	a.Cast(EventAdd, &x, 100)		// x = 130
+	a.Cast(EventAdd, &x, 100)	// x = 130
 	a.Cast(EventMultiply, &x, 3)	// x = 390
-	a.Cast(EventSquare, &x)			// x = 152100
+	a.Cast(EventSquare, &x)		// x = 152100
 
 	<-time.After(time.Second)
 	fmt.Println("x =", x)
 }
+
